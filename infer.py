0a98b95a6454b97f48c0b44fb464423de966ea5b
JiaqiangRuan <randyruan96@gmail.com>
2018-06-07 16:48:08 +0800

    update

--- infer.py
+++ infer.py
@@ -233,7 +233,7 @@ def write(params, newlines):
     with open(output_file, "a") as f:
         for line in newlines:
             f.writelines(line)
-            f.writelines()
+            f.writelines("\n")
 
 
 def main(args):
