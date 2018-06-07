2ab7cfca04a913ff838b879e6068f4773983eae4
JiaqiangRuan <randyruan96@gmail.com>
2018-06-07 22:24:56 +0800

    update

--- score.sh
+++ score.sh
@@ -4,6 +4,8 @@ PKUDICT=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/pku_training_words.utf8
 PKUGOLD=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/pku_test_gold.utf8
 MSRADICT=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/msr_training_words.utf8
 MSRAGOLD=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/msr_test_gold.utf8
+CTB6DICT=/home/rjq/project/cws/CTB6/trainSegmenter/data/ctb6.train.dict
+CTB6GOLD=/home/rjq/project/cws/CTB6/trainSegmenter/data/ctb6.test.seg
 
 DICT=$PKUDICT
 GOLD=$PKUGOLD
