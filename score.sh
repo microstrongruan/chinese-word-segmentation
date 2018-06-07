d62062ecba7b5429a745161baa2e4fafd880c8a8
JiaqiangRuan <randyruan96@gmail.com>
2018-06-07 21:46:02 +0800

    update

--- score.sh
+++ score.sh
@@ -1,2 +1,16 @@
 #!/usr/bin/env bash
 
+PKUDICT=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/pku_training_words.utf8
+PKUGOLD=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/pku_test_gold.utf8
+MSRADICT=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/msr_training_words.utf8
+MSRAGOLD=/home/rjq/project/cws/PKU&MSRA/icwb2-data/gold/msr_test_gold.utf8
+
+DICT=$PKUDICT
+GOLD=$PKUGOLD
+
+for file in `ls`
+do
+echo $file
+perl /home/rjq/project/chinese-word-segmentation/score $DICT $GOLD $file > $file-score
+tail $file-score
+done
