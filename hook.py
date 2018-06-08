0d853f62f688f8b173e9f65190691c00cfa2cafc
JiaqiangRuan <randyruan96@gmail.com>
2018-06-08 17:50:02 +0800

    update

--- hook.py
+++ hook.py
@@ -160,35 +160,29 @@ class ValidationHook(tf.train.SessionRunHook):
 
     def after_run(self, run_context, run_values):
         stale_global_step = run_values.results
-
-        if self._timer.should_trigger_for_step(stale_global_step + 1):
+        if self._timer.should_trigger_for_step(
+                stale_global_step + self._steps_per_run):
+            # get the real value after train op.
             global_step = run_context.session.run(self._global_step)
-
-            # Get the real value
             if self._timer.should_trigger_for_step(global_step):
                 self._timer.update_last_triggered_step(global_step)
+
                 # Save model
-                save_path = os.path.join(self._base_dir, "model.ckpt")
+                save_file = os.path.join(self._base_dir, "model.ckpt")
                 saver = _get_saver()
                 tf.logging.info("Saving checkpoints for %d into %s." %
-                                (global_step, save_path))
-                saver.save(run_context.session,
-                           save_path,
-                           global_step=global_step)
-                # Do validation here
+                                (global_step, save_file))
+                saver.save(run_context.session, save_file, global_step=global_step)
+
+                # Validate
                 tf.logging.info("Validating model at step %d" % global_step)
-                # score = _evaluate(self._eval_fn, self._eval_input_fn,
-                #                   self._eval_decode_fn,
-                #                   self._base_dir,
-                #                   self._session_config)
                 score = self._validate(run_context.session)
                 tf.logging.info("%s at step %d: %f" %
                                 (self._metric, global_step, score))
 
                 _save_log(self._log_name, (self._metric, global_step, score))
 
-                checkpoint_filename = os.path.join(self._base_dir,
-                                                   "checkpoint")
+                checkpoint_filename = os.path.join(self._base_dir, "checkpoint")
                 all_checkpoints = _read_checkpoint_def(checkpoint_filename)
                 records = _read_score_record(self._record_name)
                 latest_checkpoint = all_checkpoints[-1]
@@ -215,77 +209,11 @@ class ValidationHook(tf.train.SessionRunHook):
                         tf.gfile.Remove(name)
 
                 _save_score_record(self._record_name, records)
-                checkpoint_filename = checkpoint_filename.replace(
-                    self._base_dir, self._save_dir
-                )
-                _save_checkpoint_def(checkpoint_filename,
-                                     [item[0] for item in records])
+                checkpoint_filename = checkpoint_filename.replace(self._base_dir, self._save_dir)
+                _save_checkpoint_def(checkpoint_filename,[item[0] for item in records])
 
                 best_score = records[0][1]
-                tf.logging.info("Best score at step %d: %f" %
-                                (global_step, best_score))
-
-
-
-
-
-
-
-        # stale_global_step = run_values.results
-        # if self._timer.should_trigger_for_step(
-        #         stale_global_step + self._steps_per_run):
-        #     # get the real value after train op.
-        #     global_step = run_context.session.run(self._global_step)
-        #     if self._timer.should_trigger_for_step(global_step):
-        #         self._timer.update_last_triggered_step(global_step)
-        #
-        #         # Save model
-        #         save_file = os.path.join(self._base_dir, "model.ckpt")
-        #         saver = _get_saver()
-        #         tf.logging.info("Saving checkpoints for %d into %s." %
-        #                         (global_step, save_file))
-        #         saver.save(run_context.session, save_file, global_step=global_step)
-        #
-        #         # Validate
-        #         tf.logging.info("Validating model at step %d" % global_step)
-        #         score = self._validate(run_context.session)
-        #         tf.logging.info("%s at step %d: %f" %
-        #                         (self._metric, global_step, score))
-        #
-        #         _save_log(self._log_name, (self._metric, global_step, score))
-        #
-        #         checkpoint_filename = os.path.join(self._base_dir, "checkpoint")
-        #         all_checkpoints = _read_checkpoint_def(checkpoint_filename)
-        #         records = _read_score_record(self._record_name)
-        #         latest_checkpoint = all_checkpoints[-1]
-        #         record = [latest_checkpoint, score]
-        #         added, removed, records = _add_to_record(records, record,
-        #                                                  self._max_to_keep)
-        #
-        #         if added is not None:
-        #             old_path = os.path.join(self._base_dir, added)
-        #             new_path = os.path.join(self._save_dir, added)
-        #             old_files = tf.gfile.Glob(old_path + "*")
-        #             tf.logging.info("Copying %s to %s" % (old_path, new_path))
-        #
-        #             for o_file in old_files:
-        #                 n_file = o_file.replace(old_path, new_path)
-        #                 tf.gfile.Copy(o_file, n_file, overwrite=True)
-        #
-        #         if removed is not None:
-        #             filename = os.path.join(self._save_dir, removed)
-        #             tf.logging.info("Removing %s" % filename)
-        #             files = tf.gfile.Glob(filename + "*")
-        #
-        #             for name in files:
-        #                 tf.gfile.Remove(name)
-        #
-        #         _save_score_record(self._record_name, records)
-        #         checkpoint_filename = checkpoint_filename.replace(self._base_dir, self._save_dir)
-        #         _save_checkpoint_def(checkpoint_filename,[item[0] for item in records])
-        #
-        #         best_score = records[0][1]
-        #         tf.logging.info("Best score at step %d: %f" %(global_step, best_score))
+                tf.logging.info("Best score at step %d: %f" %(global_step, best_score))
 
     def end(self, session):
         pass
